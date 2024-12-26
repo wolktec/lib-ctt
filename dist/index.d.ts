7ff22e32dc2d9257cc0a979650359e278a1969eb
gbyulzz <gabriely.goncalves@analitica.ag>
2024-12-26 16:55:41 -0300

    feat: exporting interfaces

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,7 @@
+import { CttEquipment, CttEquipmentsGroupsType, CttEvent, CttAvailabilityAndAllocationResult } from "./interfaces/availabilityAllocation.interface";
+import { CttTon, CttDeliveredReturn, CttEstimatedTons, CttPartialDeliveredResult, CttWorkFronts } from "./interfaces/partialDelivered.interface";
+import { CttEquipmentProductivity, CttEquipmentProductivityFront } from "./interfaces/performanceIndicators.interface";
 export { default as createAvailabilityAllocation } from "./microservice/availabilityAllocation";
 export { default as createPartialDelivered } from "./microservice/partialDelivered";
+export { default as performanceIndicators } from "./microservice/performanceIndicators";
+export { CttEquipment, CttEquipmentsGroupsType, CttEvent, CttAvailabilityAndAllocationResult, CttTon, CttDeliveredReturn, CttEstimatedTons, CttPartialDeliveredResult, CttWorkFronts, CttEquipmentProductivity, CttEquipmentProductivityFront };
